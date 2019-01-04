eacecdc7981f51e5017b2a6b8837db9139b93967
632865163@qq.com <632865163@qq.com>
2019-01-04 12:10:19 +0800

    使用cmake构建

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Github/RegEngine
+# Build directory: D:/Github/RegEngine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
