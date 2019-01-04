9ea15b6b40a677ee52b0b466f4ffea2f38722bee
Hailiang Feng <632865163@qq.com>
2019-01-04 14:39:47 +0800

    remove build folder out of control

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Github/RegEngine
-# Build directory: D:/Github/RegEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Github/RegEngine/src
-# Build directory: D:/Github/RegEngine/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
